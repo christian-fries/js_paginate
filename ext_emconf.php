1f527a0c1ab89df742b79de048679f5db1cc18df
Christian Fries <christian.fries@lstschenker.ch>
2017-06-27 17:32:45 +0200

    Adjust composer configuration for TYPO3 8

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '3.0.3',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.0.0 - 8.9.99',
+			'typo3' => '6.0.0 - 8.7.99',
 		),
 		'conflicts' => array(
 		),
